e9df3f6483c41b6d9a2ef98c5834929f34d6ece7
edinelsonslima <edinelsonlima86@gmail.com>
2022-11-20 15:52:36 -0300

    Pagina para Dashboard

---
+++ src/Pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { Header } from '../components/Header';
+import { Orders } from '../components/Orders';
+
+export function Dashboard() {
+
+  return (
+    <>
+      <Header/>
+      <Orders />
+    </>
+  );
+}
